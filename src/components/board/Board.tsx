fd6aa44cf6be01276fdbc3057346b5b76724b2d2
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2022-12-20 10:28:31 -0300

    Commit 1: Game logic working

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -8,7 +8,7 @@ import { useState } from "react";
 import styles from "./Board.module.css";
 import RoundButton from "../round-button/RoundButton";
 
-const myCards: number[] = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5];
+const myCards: number[] = [1, 2, 3, 4, 5, 6];
 
 const myBoard: number[][] = [
   [0, 0, 0, 0],
@@ -16,14 +16,19 @@ const myBoard: number[][] = [
   [0, 0, 0, 0],
 ];
 
-const hiddenBoard: boolean[][] = [
-  [false, false, false, false],
-  [false, false, false, false],
-  [false, false, false, false],
+const shownBoard: boolean[][] = [
+  [true, true, true, true],
+  [true, true, true, true],
+  [true, true, true, true],
 ];
 
+type coordinates = {
+  row: number;
+  column: number;
+}
+
 const cardsShuffle = (): number[][] => {
-  let cards = myCards;
+  let cards = [...myCards, ...myCards];
   let board = myBoard;
   for (var i = cards.length - 1; i > 0; i--) {
     var j = Math.floor(Math.random() * (i + 1));
@@ -43,17 +48,43 @@ const cardsShuffle = (): number[][] => {
 
 export default function Board() {
   const [currentBoard, setCurrentBoard] = useState<number[][]>(cardsShuffle);
-  const [revealedBoard, setRevealedBoard] = useState<boolean[][]>(hiddenBoard);
+  const [firstCard, setFirstCard] = useState<number[] | undefined>();
+  const [revealedBoard, setRevealedBoard] = useState<boolean[][]>(
+    new Array(currentBoard.length)
+      .fill("")
+      .map(() => new Array(currentBoard[0].length).fill(false))
+  );
 
-  const onClickHandler = () => {};
-
-  const revealCards = () => {
-    for (let row = 0; row < 3; row++) {
-      for (let col = 0; col < 4; col++) {
-        if ((revealedBoard[row][col] = true)) {
-        }
-      }
+  const onClickHandler = (rowId: number, colId: number) => {
+    const clickedCard = currentBoard[rowId][colId];
+    //If card was already selected
+    if (revealedBoard[rowId][colId] === true) {
+      console.log(`Invalid card`);
+      return;
     }
+    const copyRevealedBoard = [...revealedBoard];
+    copyRevealedBoard[rowId][colId] = true;
+    setRevealedBoard(copyRevealedBoard);
+      //If first card hasn't been set yet
+      if (!firstCard) {
+        setFirstCard([rowId, colId]);
+
+        //If clicked card equals to first card
+      } else {
+        if (clickedCard === currentBoard[firstCard[0]][firstCard[1]]) {
+          console.log(clickedCard);
+          console.log(currentBoard[firstCard[0]][firstCard[1]])
+          //If clicked card differs from first card
+        } else {
+          setTimeout(() => {
+            const newRevealedBoard = [...revealedBoard]
+            newRevealedBoard[rowId][colId] = false;
+            newRevealedBoard[firstCard[0]][firstCard[1]] = false;
+            setRevealedBoard(newRevealedBoard);
+          }, 1000);
+        }
+        setFirstCard(undefined);
+      }
   };
 
   return (
@@ -63,7 +94,11 @@ export default function Board() {
           <div className={styles.row} key={rowId}>
             {row.map((card, colId) => (
               <div className={styles.card} key={colId}>
-                <Card value={card} onClickHandler={onClickHandler}></Card>
+                <Card
+                  value={card}
+                  onClickHandler={() => onClickHandler(rowId, colId)}
+                  isVisible={revealedBoard[rowId][colId]}
+                ></Card>
               </div>
             ))}
           </div>
--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -1,15 +1,16 @@
 // CSS
 import styles from "./Card.module.css";
-
 interface ICardProps {
   value: number;
   onClickHandler: React.MouseEventHandler;
+  isVisible: boolean
 }
 
-export default function Card({ value, onClickHandler }: ICardProps) {
+export default function Card({ value, onClickHandler, isVisible }: ICardProps) {
+
   return (
-    <div className={styles.card}>
-      <div className={`${styles.face} ${styles.hidden}`} onClick={onClickHandler} id={`#${value}`}>
+    <div className={styles.card} onClick={onClickHandler}>
+      <div className={isVisible ? styles.face : `${styles.face} ${styles.hidden}`}  id={`#${value}`}>
         <h1>{value}</h1>
       </div>
     </div>

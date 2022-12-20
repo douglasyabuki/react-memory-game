27d1d3bf8d41e7e6a5700e1900600e0ece2cea4d
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2022-12-20 18:47:31 -0300

    Commit 3: Add images and cleaner code

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -10,10 +10,8 @@ export default function Card({ value, onClickHandler, isVisible }: ICardProps) {
   return (
     <div className={styles.cardContainer} onClick={onClickHandler}>
       <div className={isVisible? styles.card :`${styles.card} ${styles.flipped}`}>
-        <div className={`${styles.front} ${styles.cardFace}`} id={`#${value}`}>
-          <h1>{value}</h1>
-        </div>
-        <div className={`${styles.back} ${styles.cardFace}`}></div>
+        <img className={`${styles.front} ${styles.cardFace}`} id={`#${value}`} src={`card${value}.jpg`}/>
+        <img className={`${styles.back} ${styles.cardFace}`} src={"cardback.jpg"}/>
       </div>
     </div>
   );

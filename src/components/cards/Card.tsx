b2e50ed2ce4dbe23734abcfd4589e9de1f294b80
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2022-12-20 14:28:11 -0300

    Commit 2: Added style and animation to cards

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -3,15 +3,17 @@ import styles from "./Card.module.css";
 interface ICardProps {
   value: number;
   onClickHandler: React.MouseEventHandler;
-  isVisible: boolean
+  isVisible: boolean;
 }
 
 export default function Card({ value, onClickHandler, isVisible }: ICardProps) {
-
   return (
-    <div className={styles.card} onClick={onClickHandler}>
-      <div className={isVisible ? styles.face : `${styles.face} ${styles.hidden}`}  id={`#${value}`}>
-        <h1>{value}</h1>
+    <div className={styles.cardContainer} onClick={onClickHandler}>
+      <div className={isVisible? styles.card :`${styles.card} ${styles.flipped}`}>
+        <div className={`${styles.front} ${styles.cardFace}`} id={`#${value}`}>
+          <h1>{value}</h1>
+        </div>
+        <div className={`${styles.back} ${styles.cardFace}`}></div>
       </div>
     </div>
   );
